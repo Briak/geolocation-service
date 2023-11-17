970cf85cd611a438428e93e66fa43487e770d3b5
Kristina Efros <kristina.efros@gmail.com>
2023-11-17 08:31:22 +0100

    Update response format for Flickr API

--- app/src/main/java/com/kristinaefros/challenge/data/network/response/PhotosResponse.kt
+++ app/src/main/java/com/kristinaefros/challenge/data/network/response/PhotosResponse.kt
@@ -4,5 +4,5 @@ import com.google.gson.annotations.SerializedName
 import com.kristinaefros.challenge.data.network.dto.PhotoDto
 
 data class PhotosResponse(
-    @SerializedName("photo") val photo: List<PhotoDto>,
+    @SerializedName("photo") val photo: List<PhotoDto>?,
 )
